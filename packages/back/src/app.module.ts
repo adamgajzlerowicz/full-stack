481ae2206f0dc5bf79b66059cc62d03b9a9dc89f
Adam Gajzlerowicz <nelf86@gmail.com>
2019-06-02 16:38:32 +0200

    meh

--- packages/back/src/app.module.ts
+++ packages/back/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://db/rest'), UserModule],
+  imports: [MongooseModule.forRoot('mongodb://db/rest'), UserModule, AuthModule],
 
   controllers: [AppController],
   providers: [AppService, AuthService],
--- packages/back/src/user/user.module.ts
+++ packages/back/src/user/user.module.ts
@@ -7,9 +7,8 @@ import { UserSchema } from './schemas/user.schema';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
-
   controllers: [UserController],
   providers: [UserService],
-  exports: [UserService]
+  exports: [UserService],
 })
 export class UserModule {}
