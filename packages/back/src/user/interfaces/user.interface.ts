3c8739ef022398065d6ed2ad7f29cf1ce6d2032b
Adam Gajzlerowicz <nelf86@gmail.com>
2019-06-02 16:23:23 +0200

    meh

---
+++ packages/back/src/user/interfaces/user.interface.ts
@@ -0,0 +1,7 @@
+import {read} from "fs";
+
+export interface User {
+    name: string;
+    password: string;
+    email: string;
+}
---
+++ packages/back/src/user/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+
+import { User } from './interfaces/user.interface';
+
+@Injectable()
+export class UserService {
+    private readonly users: User[] = [];
+
+    create(user: User) {
+        this.users.push(user);
+    }
+
+    findAll() {
+        return this.users;
+    }
+}
